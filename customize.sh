f8ec34161cd8281766bba2316801ea798a2976c8
avef1000 <168319647+avef1000@users.noreply.github.com>
2024-07-30 13:11:49 -0400

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,70 +1,52 @@
-##########################################################################################
-#
-# MMT Extended Config Script
-#
-##########################################################################################
+#!/data/adb/magisk/busybox sh
+MODDIR=${0%/*}
 
-##########################################################################################
-# Config Flags
-##########################################################################################
+# Ensure ASH standalone mode
+export ASH_STANDALONE=1
 
-# Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maximum android version for your mod
-# Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
-# Uncomment PARTOVER if you have a workaround in place for extra partitions in regular magisk install (can mount them yourself - you will need to do this each boot as well). If unsure, keep commented
-# Uncomment PARTITIONS and list additional partitions you will be modifying (other than system and vendor), for example: PARTITIONS="/odm /product /system_ext"
-#MINAPI=21
-#MAXAPI=25
-#DYNLIB=true
-#PARTOVER=true
-#PARTITIONS=""
-
-##########################################################################################
-# Replace list
-##########################################################################################
-
-# List all directories you want to directly replace in the system
-# Check the documentations for more info why you would need this
-
-# Construct your list in the following format
-# This is an example
-REPLACE_EXAMPLE="
-/system/app/Youtube
-/system/priv-app/SystemUI
-/system/priv-app/Settings
-/system/framework
-"
-
-# Construct your own list here
-REPLACE="
-"
-
-##########################################################################################
-# Permissions
-##########################################################################################
-
-set_permissions() {
-  : # Remove this if adding to this function
-
-  # Note that all files/folders in magisk module directory have the $MODPATH prefix - keep this prefix on all of your files/folders
-  # Some examples:
-  
-  # For directories (includes files in them):
-  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
-  
-  # set_perm_recursive $MODPATH/system/lib 0 0 0755 0644
-  # set_perm_recursive $MODPATH/system/vendor/lib/soundfx 0 0 0755 0644
-
-  # For files (not in directories taken care of above)
-  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
-  
-  # set_perm $MODPATH/system/lib/libart.so 0 0 0644
-  # set_perm /data/local/tmp/file.txt 0 0 644
+# Verbose logging
+ui_print() {
+  echo "$1"
 }
 
-##########################################################################################
-# MMT Extended Logic - Don't modify anything after this
-##########################################################################################
+# Create directory for backups
+mkdir -p /sdcard/theRipper
 
-SKIPUNZIP=1
-unzip -qjo "$ZIPFILE" 'common/functions.sh' -d $TMPDIR >&2
-. $TMPDIR/functions.sh
+# Trigger backup if requested
+if [ -f /data/adb/magisk/backup_trigger ]; then
+  ui_print "Initialization..."
+
+  BOOT_PART=$(ls /dev/block/by-name/boot*)
+  RECOVERY_PART=$(ls /dev/block/by-name/recovery*)
+  if [ -n "$BOOT_PART" ]; then
+    ui_print "Found boot partition: $BOOT_PART"
+    dd if=$BOOT_PART of=/sdcard/theRipper/boot.img
+    ui_print "Boot image copied to /sdcard/theRipper/boot.img"
+  else
+    ui_print "Boot partition not found!"
+  fi
+  
+  if [ -n "$RECOVERY_PART" ]; then
+    ui_print "Found recovery partition: $RECOVERY_PART"
+    dd if=$RECOVERY_PART of=/sdcard/theRipper/recovery.img
+    ui_print "Recovery image copied to /sdcard/theRipper/recovery.img"
+  else
+    ui_print "Recovery partition not found!"
+  fi
+  
+  if [ -n "$BOOT_PART" ]; then
+    ui_print "Backing up kernel..."
+    cd /data/local/tmp
+    dd if=$BOOT_PART of=boot.img
+    /data/adb/magisk/magiskboot unpack boot.img
+    mv kernel zimage
+    cp zimage /sdcard/theRipper/
+    /data/adb/magisk/magiskboot cleanup
+    rm -r boot.img
+    ui_print "Kernel copied to /sdcard/theRipper/zimage"
+  else
+    ui_print "Boot partition not found for kernel backup!"
+  fi
+  
+  rm /data/adb/magisk/backup_trigger
+fi
